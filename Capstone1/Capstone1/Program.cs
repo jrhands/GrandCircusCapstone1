b989e060ab27a886c89baa419a3e383ac604bc05
Jacob Hands <jrhands@gmail.com>
2018-07-21 18:30:24 -0400

    Splits word into two parts successfully

---
+++ Capstone1/Capstone1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace Capstone1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Translate(GetInput());
+            } while (Continue());
+            Console.Read();
+        }
+
+        static String GetInput()
+        {
+            Console.WriteLine("Input a word");
+            return Console.ReadLine();
+        }
+
+        static void Translate(String input)
+        {
+            String vowels = "aeiouAEIOU";
+            int characterPos = -1;
+            foreach (char character in input)
+            {
+                characterPos += 1;
+                if (vowels.Contains(character) || (characterPos >= 1 && (character == 'y' || character == 'Y')))
+                {
+                    break;
+                }
+            }
+            if (characterPos >= 0)
+            {
+                Console.WriteLine(input.Substring(0, characterPos));
+                Console.WriteLine(input.Substring(characterPos, input.Length - characterPos));
+            }
+        }
+
+        static bool Continue()
+        {
+            while (true)
+            {
+                Console.Write("Continue? (y/n): ");
+                String input = Console.ReadLine().ToLower();
+                if (input == "y" || input == "yes")
+                {
+                    return true;
+                }
+                else if (input == "n" || input == "no")
+                {
+                    return false;
+                }
+                Console.WriteLine("Invalid input.");
+            }
+        }
+    }
+}
